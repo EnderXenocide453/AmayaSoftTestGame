cda6af46287dd49b6c948597b010355ea27c70fb
Lakssterius <lakssterius@gmail.com>
2024-03-25 16:40:14 +0300

    Логика управления уровнями вынесена в отдельный класс. Блокировка кнопок при победе

--- Assets/Scripts/GridExecutionHandler.cs
+++ Assets/Scripts/GridExecutionHandler.cs
@@ -1,46 +1,39 @@
 using UnityEngine;
-using GridManagement.Data;
-using GridManagement.Generation;
 using GridManagement.UI;
-using System.Collections;
 using System;
-using UnityEngineInternal;
 
 namespace GridManagement
 {
+    [RequireComponent(typeof(LevelsIterator))]
     public class GridExecutionHandler : MonoBehaviour
     {
-        [Header("Data")]
-        [SerializeField] private GridCellsData m_CellsData;
-        [SerializeField] private GridSizeData[] m_GridLevels;
         [Header("Logic")]
-        [SerializeField] private BaseGridGenerator m_GridGenerator;
-        [SerializeField] private BaseGridVisualizer m_GridVisualizer;
-        [SerializeField] private BaseQuestionVisualizer m_QuestionVisualizer;
-        [Header("Misc.")]
-        [SerializeField] private float m_LevelPassDelay = 1f;
+        [SerializeField] private LevelsIterator m_Iterator;
 
-        private int m_CurrentLevel;
-        private bool m_FirstInit = true;
-        private GridGenerationData m_GridData;
+        private bool m_ISFirstInit = true;
+        private bool m_IsButtonsActive = true;
 
-        public bool IsFirstInit => m_FirstInit;
+        public bool IsFirstInit => m_ISFirstInit;
 
         public event Action onWin;
 
         private void Start()
         {
-            if (!ValidateData())
-                return;
+            m_Iterator ??= GetComponent<LevelsIterator>();
+            m_Iterator.onIterationEnds += Win;
 
-            InitLevel(0);
+            m_Iterator.InitLevel(0);
+            m_ISFirstInit = false;
         }
 
         public void OnCellSelected(GridCellButton cellButton)
         {
-            if (cellButton.Index == m_GridData.CorrectIndex) {
+            if (!m_IsButtonsActive)
+                return;
+
+            if (m_Iterator.CheckIndex(cellButton.Index)) {
                 cellButton.Animation.PlayApplyAnimation();
-                PassLevel();
+                m_Iterator.PassLevel();
 
                 return;
             }
@@ -50,76 +43,14 @@ namespace GridManagement
 
         public void Restart()
         {
-            InitLevel(0);
-        }
-
-        private void InitLevel(int id)
-        {
-            id = Mathf.Clamp(id, 0, m_GridLevels.Length - 1);
-
-            m_GridData = m_GridGenerator.GenerateGrid(m_CellsData, m_GridLevels[id]);
-            m_GridVisualizer.VisualizeGrid(m_GridData);
-            m_QuestionVisualizer.VisualizeQuestion(m_GridData.CorrectCell.Name);
-
-            m_CurrentLevel = id;
-            m_FirstInit = false;
-        }
-
-        private void InitLevel(int id, GridCellsData gridCellsData)
-        {
-            m_CellsData = gridCellsData;
-            InitLevel(id);
-        }
-
-        private void PassLevel()
-        {
-            StartCoroutine(WaitAndPass(m_LevelPassDelay));
-        }
-
-        private void LoadNextLevel()
-        {
-            if (++m_CurrentLevel >= m_GridLevels.Length) {
-                Win();
-                return;
-            }
-
-            InitLevel(m_CurrentLevel);
+            m_Iterator.InitLevel(0);
+            m_IsButtonsActive = true;
         }
 
         private void Win()
         {
+            m_IsButtonsActive = false;
             onWin?.Invoke();
-        }
-
-        private bool ValidateData()
-        {
-            bool isValid = true;
-
-            if (m_CellsData == null) {
-                Debug.LogError("Не указаны данные наполнения ячеек!");
-                isValid = false;
-            } if (m_GridLevels == null) {
-                Debug.LogError("Не указаны данные уровней!");
-                isValid = false;
-            } if (m_GridGenerator == null) {
-                Debug.LogError("Не назначен генератор сетки!");
-                isValid = false;
-            } if (m_GridVisualizer == null) {
-                Debug.LogError("Не назначен визуализатор сетки!");
-                isValid = false;
-            } if (m_QuestionVisualizer == null) {
-                Debug.LogError("Не назначен визуализатор вопроса!");
-                isValid = false;
-            }
-
-            return isValid;
-        }
-
-        private IEnumerator WaitAndPass(float delay)
-        {
-            yield return new WaitForSeconds(delay);
-
-            LoadNextLevel();
         }
     }
 }
---
+++ Assets/Scripts/LevelsIterator.cs
@@ -0,0 +1,107 @@
+using GridManagement.Data;
+using GridManagement.Generation;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace GridManagement
+{
+    public class LevelsIterator : MonoBehaviour
+    {
+        [Header("Data")]
+        [SerializeField] private GridCellsData m_CellsData;
+        [SerializeField] private GridSizeData[] m_GridLevels;
+        [Header("Logic")]
+        [SerializeField] private BaseGridGenerator m_GridGenerator;
+        [SerializeField] private BaseGridVisualizer m_GridVisualizer;
+        [SerializeField] private BaseQuestionVisualizer m_QuestionVisualizer;
+        [Header("Misc.")]
+        [SerializeField] private float m_LevelPassDelay = 1f;
+
+        private int m_CurrentLevel;
+        private GridGenerationData m_GridData;
+
+        public event Action onIterationEnds;
+
+        private void Start()
+        {
+            if (!ValidateData())
+                return;
+        }
+
+        public void InitLevel(int id)
+        {
+            id = Mathf.Clamp(id, 0, m_GridLevels.Length - 1);
+
+            m_GridData = m_GridGenerator.GenerateGrid(m_CellsData, m_GridLevels[id]);
+            m_GridVisualizer.VisualizeGrid(m_GridData);
+            m_QuestionVisualizer.VisualizeQuestion(m_GridData.CorrectCell.Name);
+
+            m_CurrentLevel = id;
+        }
+
+        public void InitLevel(int id, GridCellsData gridCellsData)
+        {
+            m_CellsData = gridCellsData;
+            InitLevel(id);
+        }
+
+        public void PassLevel()
+        {
+            StartCoroutine(WaitAndPass(m_LevelPassDelay));
+        }
+
+        public bool CheckIndex(Vector2Int index) => index == m_GridData.CorrectIndex;
+        
+        private void LoadNextLevel()
+        {
+            if (++m_CurrentLevel >= m_GridLevels.Length) {
+                Win();
+                return;
+            }
+
+            InitLevel(m_CurrentLevel);
+        }
+
+        private void Win()
+        {
+            onIterationEnds?.Invoke();
+        }
+
+        private IEnumerator WaitAndPass(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+
+            LoadNextLevel();
+        }
+
+        private bool ValidateData()
+        {
+            bool isValid = true;
+
+            if (m_CellsData == null) {
+                Debug.LogError("Не указаны данные наполнения ячеек!");
+                isValid = false;
+            }
+            if (m_GridLevels == null) {
+                Debug.LogError("Не указаны данные уровней!");
+                isValid = false;
+            }
+            if (m_GridGenerator == null) {
+                Debug.LogError("Не назначен генератор сетки!");
+                isValid = false;
+            }
+            if (m_GridVisualizer == null) {
+                Debug.LogError("Не назначен визуализатор сетки!");
+                isValid = false;
+            }
+            if (m_QuestionVisualizer == null) {
+                Debug.LogError("Не назначен визуализатор вопроса!");
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+}
+
